aafd4665d193a4133e6b2bc8bd75e33da8ac815a
Arturo Quirós <joseq2408@gmail.com>
2022-11-28 20:42:41 -0600

    go

---
+++ src/main/java/org/arturoquiros/springcloud/msvc/usuarios/msvcusuarios/MsvcUsuariosApplication.java
@@ -0,0 +1,13 @@
+package org.arturoquiros.springcloud.msvc.usuarios.msvcusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcUsuariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/arturoquiros/springcloud/msvc/usuarios/msvcusuarios/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arturoquiros.springcloud.msvc.usuarios.msvcusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
